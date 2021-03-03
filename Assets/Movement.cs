3de9c91c217f16346c9f3eaecb137a46b3404dd1
msp <matt@cenatus.org>
2021-03-03 18:12:08 +0100

    Update beam movement script to adjust spot angle

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public GameObject target;
+    private VLB.VolumetricLightBeam beam;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        beam = GetComponent<VLB.VolumetricLightBeam>();
     }
 
    void Update()
     {
-        transform.RotateAround(target.transform.position, Vector3.down, 20 * Time.deltaTime);
+        // transform.RotateAround(target.transform.position, Vector3.down, 20 * Time.deltaTime);
+        // beam.fallOffEnd = Time.time * 0.1f;
+        // beam.color = Color.red;
+        beam.spotAngle = Time.time * 0.3f;
+        beam.UpdateAfterManualPropertyChange(); // take the changes into account
     }
 
 
