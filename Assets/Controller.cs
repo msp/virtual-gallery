916fc6593794f9b4c59668cd8b9b19881b239c1b
msp <matt@cenatus.org>
2021-03-03 13:38:52 +0100

    Turn off the editor lighting on startup

---
+++ Assets/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Light editorLighting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        editorLighting = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        editorLighting.enabled = false;
+    }
+}
