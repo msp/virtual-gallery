3b32f77ee088d0e3bdc26630b7b378fcc19835af
msp <matt@cenatus.org>
2021-03-03 13:16:32 +0100

    Add a Quest compatible volumetric light with some simple movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+   void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.down, 20 * Time.deltaTime);
+    }
+
+
+
+}
