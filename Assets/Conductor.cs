26a7d39ffd428103990545abe6a3bd157fffd25d
msp <matt@cenatus.org>
2021-03-03 22:24:00 +0100

    Test out some sound with Audio Helm

---
+++ Assets/Conductor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    // audio
+    public AudioHelm.HelmController helmController;
+    public int note = 41;
+    public float velocity = 0.95f;
+    public int duration = 6;
+
+    // visual
+    public GameObject lightBeam;
+    private VLB.VolumetricLightBeam beam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beam = lightBeam.GetComponent<VLB.VolumetricLightBeam>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!helmController.IsNoteOn(note))
+        {
+            helmController.NoteOn(note, velocity, duration);
+        }
+
+        // helmController.SetParameterPercent(AudioHelm.Param.kFilterEnvelopeDepth, (Time.time * 0.3f)/100);
+
+
+        beam.spotAngle = Time.time * 0.1f;
+        beam.UpdateAfterManualPropertyChange();
+
+
+
+
+
+
+
+    }
+}
